4233daccf2c00f7fe417d1266bf2c6a3afb44a9f
Phzin <60586254+Kuuh4k0@users.noreply.github.com>
2024-02-29 17:00:27 -0300

    Add files via upload

---
+++ controle.php
@@ -0,0 +1,63 @@
+<?php
+
+/* $acao = filter_input(INPUT_POST, 'acao', FILTER_SANITIZE_STRING);
+$acaoid = filter_input(INPUT_POST, 'acaoid', FILTER_SANITIZE_NUMBER_INT);
+$controle = filter_input(INPUT_POST, 'controle', FILTER_SANITIZE_STRING);
+$controleGet = filter_input(INPUT_POST, 'controleGet', FILTER_SANITIZE_STRING);
+
+switch ($acao) {
+
+}
+switch ($controle) {
+    case 'listaCliente':
+        include_once 'cliente.php';
+        break;
+    case 'listaGenero':
+        include_once 'cliente.php';
+        break;
+    case 'listaFilme':
+        include_once 'cliente.php';
+        break;
+}
+switch ($acao) {
+
+} */
+$controle = filter_input(INPUT_POST, 'controle', FILTER_SANITIZE_STRING);
+
+switch ($controle) {
+    case 'listaGenero':
+        include_once './adm/generos.php';
+        break;
+    case 'listaFilmes':
+        include_once './adm/filmes.php';
+        break;
+    case 'listaCliente':
+        include_once './adm/cliente.php';
+        break;
+    case 'generoAdd':
+        include_once './generoadd.php';
+        break;
+    case 'listaCliente':
+        include_once './adm/cliente.php';
+        break;
+    default:
+        echo 'Lista Inexistente!';
+        break;
+}
+
+
+/* if (isset($controle) && !empty($controle)){
+    $controle = $_POST['controle'];
+    if ($controle == 'listaGenero') {
+        echo 'Você clicou no botão listar Gênero!';
+    } else if ($controle == 'listaFilmes'){
+        echo 'Você clicou no botão listar Filmes!';
+    }
+    else if ($controle == 'listaCliente'){
+        echo 'Você clicou no botão listar Cliente!';
+    } else {
+    echo 'Menu não disponível!!!';
+    }
+    } else {
+        echo 'Página ineistente!!!';
+    } */
---
+++ filmeadd.php
@@ -0,0 +1,26 @@
+<?php
+include_once "config/conexao.php";
+include_once "config/constantes.php";
+include_once "func/funcoes.php";
+$conn = conectar();
+
+$Dados = filter_input_array(INPUT_POST, FILTER_DEFAULT);
+
+// echo json_encode([$Dados['genero']]);
+
+if (isset($Dados) && !empty($Dados)){
+    $genero = isset($Dados["genero"]) ? addslashes(mb_strtoupper($Dados["genero"], 'UTF-8')) : '';
+
+    $retornoInsert = insertUnico('genero', 'genero',$genero);
+  
+    if ($retornoInsert > 0) {
+        echo json_encode(['success' => true, 'message' => "Genero $genero cadastrado com successo!"],JSON_THROW_ON_ERROR);
+    } else {
+        echo json_encode(['success' => false, 'message' => "Genero não cadastrado! ErroR Bd"],JSON_THROW_ON_ERROR);
+    }
+} else {
+  
+    echo json_encode(['success' => false, 'message' => "Genero Não cadastrado! ErroR variável"],JSON_THROW_ON_ERROR);
+}
+
+?>
